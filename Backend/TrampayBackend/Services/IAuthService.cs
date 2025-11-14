8a7ab2bc1fa7067aead25c48fc18f66efffd1a45
Tales de Souza <talesjardim08@gmail.com>
2025-11-14 03:25:26 +0000

    Bug

--- Backend/TrampayBackend/Services/IAuthService.cs
+++ Backend/TrampayBackend/Services/IAuthService.cs
@@ -5,11 +5,10 @@ namespace TrampayBackend.Services
 {
     public interface IAuthService
     {
-        (string Hash, string Salt) HashPassword(string password, string? salt = null);
-        bool VerifyPassword(string password, string hash, string salt);
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string hash);
         string GenerateJwtToken(long userId, string? email, string? role = "user");
 
-        // === novos métodos ===
         Task<(bool Success, string Message, string? Token)> AuthenticateAsync(string email, string password);
         Task<(bool Success, string Message, string? Token)> RegisterAsync(User newUser, string password);
     }
